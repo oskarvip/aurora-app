4d5166bcfee2c2a9f660290111db2e1afb24196c
Oskar Enhörning <oskar.enhorning@gmail.com>
2021-02-10 09:49:46 +0100

    testing black

--- src/aurora-app.py
+++ src/aurora-app.py
@@ -13,11 +13,14 @@ def split_list(lst):
     ratings = []
     for row in lst:
         coords.append((row[0], row[1]))
+
+
+
+
+
+
         ratings.append(row[2])
     return coords, ratings
-
-
-# aurora_coords, aurora_ratings = split_list(data['coordinates'])
 
 
 @st.cache
@@ -38,26 +41,16 @@ aurora_data = load_data()
 st.header("Enter your location")
 my_pos_input = "(22.154751, 65.584160)"
 
-# sequence = st.sidebar.text_area("Sequence input", my_pos, height=250)
 my_pos = st.text_area(
-    "Input coordinates on the form (long, lat)", my_pos_input, height=250
+    "Input coordinates on the form (long, lat)", my_pos_input, height=50
 )
 my_pos = eval(my_pos)
-# st.write(my_pos)
 
-
-# st.write(aurora_data.keys())
 aurora_coords, aurora_ratings = split_list(aurora_data["coordinates"])
-# st.write(aurora_coords)
+
 tree = spatial.KDTree(aurora_coords)
-
-
-# my_pos = (coordinates["long"], coordinates["lat"])
 dist, index = tree.query(my_pos)
 
-# print(my_pos, aurora_coords[index], aurora_ratings[index])
-
-# my_pos = {"lat": 65.584160, "long": 22.154751}
 ret = {
     "my_position": my_pos,
     "distance": dist,
