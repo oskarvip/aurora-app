1b702c6045f84ee6d05058692d3ccdb968670064
Oskar Enhörning <oskar.enhorning@gmail.com>
2021-02-11 10:09:09 +0100

    forward geocoding using mapbox added to app

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
+echo "Removing current aurora-app"
+docker stop aurora-app
+docker rm aurora-app
+echo "Starting up the aurora-app"
 docker run \
 --name aurora-app \
 --mount type=bind,source="$(pwd)"/src,target=/code \
+--env-file "$(pwd)"/.env \
 -p 8080:80 \
 -d \
 aurora-app
--- src/aurora-app.py
+++ src/aurora-app.py
@@ -2,10 +2,37 @@ import streamlit as st
 import json
 from urllib.request import urlopen
 from scipy import spatial
+from os import environ
+from mapbox import Geocoder
+
+# Declare constants
+SCANDINAVIA_BBOX = (0.105400, 53.944367, 32.712822, 72.148786)
+SCANDINAVIA_CENTER = (16.347656, 64.510643)
+AURORA_DATA_URL = "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json"
+MAPBOX_ACCESS_TOKEN = environ.get("MAPBOX_ACCESS_TOKEN")
+if not MAPBOX_ACCESS_TOKEN:
+    raise ValueError("Missing MAPBOX_ACCESS_TOKEN")
+geocoder = Geocoder()
 
 
 st.title("Aurora Predictor app")
 st.write("Predictions are performed using OVATION Prime auroral precipitation model")
+# st.write("MAPBOX TOKEN IS: " + str(MAPBOX_ACCESS_TOKEN))
+
+
+def forward_geocode(query):
+    """
+    Takes a query and returns the center coordinates as a tuple
+    Args:
+        query: str containing the name of a place
+    """
+    response = geocoder.forward(
+        query, limit=3, types=["country", "place", "address"]
+    ).json()
+    features = response["features"]
+    if len(features) == 0:
+        return "Your search query yielded no results"
+    return features[0]
 
 
 def split_list(lst):
@@ -19,38 +46,43 @@ def split_list(lst):
 
 
 @st.cache
-def load_data():
-    url = "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json"
+def load_data(url):
     data = urlopen(url).read().decode()
     obj = json.loads(data)
     return obj
 
 
-def find_closest(my_pos):
-    dist, index = tree.query(my_pos)
+def find_closest(position):
+    dist, index = tree.query(position)
     return dist, index
 
 
-aurora_data = load_data()
-
-st.header("Enter your location")
-my_pos_input = "(22.154751, 65.584160)"
-
-my_pos = st.text_area(
-    "Input coordinates on the form (long, lat)", my_pos_input, height=50
-)
-my_pos = eval(my_pos)
-
+# Download the aurora predictions and prepare for search
+aurora_data = load_data(AURORA_DATA_URL)
 aurora_coords, aurora_ratings = split_list(aurora_data["coordinates"])
-
 tree = spatial.KDTree(aurora_coords)
-dist, index = tree.query(my_pos)
 
+# Get the users location and convert to coordinates
+st.header("Enter your location")
+position_input = "Kiruna"
+position_query = st.text_area("Enter a place (ex Kiruna)", "Kiruna", height=50)
+
+position_properties = forward_geocode(position_query)
+
+
+dist, index = tree.query(position_properties["center"])
 ret = {
-    "my_position": my_pos,
+    "query": position_query,
+    "query_result": position_properties["place_name"],
+    "position": position_properties["center"],
     "distance": dist,
     "aurora_coords": aurora_coords[index],
-    "aurora_ratings": aurora_ratings[index],
+    "observation_time": aurora_data["Observation Time"],
+    "forecast_time": aurora_data["Forecast Time"],
+    "aurora_probability": str(aurora_ratings[index]) + "%",
 }
-
+st.write(
+    "Forecasts should be interpreted as the probability of observing an aurora "
+    "directly above you up to an hour in the future"
+)
 st.write(ret)
